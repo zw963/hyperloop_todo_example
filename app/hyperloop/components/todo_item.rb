780d7884bdb74c7e4dcda20f302702e5745f92f0
Billy.Zheng <zw963@163.com>
2017-07-29 13:36:48 +0800

    给 checkbox 的点击增加 handler, 在 handler 中改变 todo checked 状态.

--- app/hyperloop/components/todo_item.rb
+++ app/hyperloop/components/todo_item.rb
@@ -1,7 +1,9 @@
 class TodoItem < Hyperloop::Component
   param :todo
   render(LI) do
-    INPUT(type: :checkbox, checked: params.todo.completed)
+    INPUT(type: :checkbox, checked: params.todo.completed).on(:click) do |evt|
+      params.todo.update(completed: !params.todo.completed)
+    end
     params.todo.title
   end
 end
