f03e10c66782a152cfdcb1e77521c70debf7f33d
Billy.Zheng <zw963@163.com>
2017-07-30 01:09:15 +0800

    Fix footer, it worked.

--- app/hyperloop/components/footer.rb
+++ app/hyperloop/components/footer.rb
@@ -4,7 +4,7 @@ class Footer < Hyperloop::Component
     # 注意这里的 css 属性, marge-right, 写法是类似于 JS 的写法,
     # 这样的做法足够特殊, 让我们可以立即知道, 哦, 这是一个 CSS 属性!
     # 然后, 10px,  简写为 10.
-    NavLink(href: "/#{path}", style: {marginRight: 10}) { path.camelize }
+    NavLink("/#{path}", style: {marginRight: 10}) { path.camelize }
   end
 
   render(DIV) do
