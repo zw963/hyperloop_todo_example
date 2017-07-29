d1450c921bf0c4bc5c94232ebb0a361d8aaf7bba
Billy.Zheng <zw963@163.com>
2017-07-30 00:50:59 +0800

    update footer, seem like not work

--- app/hyperloop/components/footer.rb
+++ app/hyperloop/components/footer.rb
@@ -1,5 +1,15 @@
 class Footer < Hyperloop::Component
+  include HyperRouter::ComponentMethods
+  def link_item(path)
+    # 注意这里的 css 属性, marge-right, 写法是类似于 JS 的写法,
+    # 这样的做法足够特殊, 让我们可以立即知道, 哦, 这是一个 CSS 属性!
+    # 然后, 10px,  简写为 10.
+    NavLink(href: "/#{path}", style: {marginRight: 10}) { path.camelize }
+  end
+
   render(DIV) do
-    'Footer will go here'
+    link_item(:all)
+    link_item(:active)
+    link_item(:completed)
   end
 end
