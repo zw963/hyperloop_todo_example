01df895838e4c43628bd48a6b53ceafae40f9b84
Billy.Zheng <zw963@163.com>
2017-07-30 23:07:17 +0800

    在 Header 中调用 EditItem, 试图添加一个新的 Todo. 但是不工作:
    因为 React 对待 INPUT 元素比较特殊, 它仅仅在 INPUT 元素第一次 mount
    的时候加载一次. 解决的办法是: 添加一个 key: 属性, 告诉 react 使用这个
    来确定唯一性.

--- app/hyperloop/components/header.rb
+++ app/hyperloop/components/header.rb
@@ -1,5 +1,11 @@
 class Header < Hyperloop::Component
+  state(:new_todo) { Todo.new }
+
   render(HEADER) do
-    'Header will go here'
+    EditItem(
+      todo:      state.new_todo,
+      on_save:   proc { puts 'hello'; mutate.new_tudo Todo.new },
+      on_cancel: proc {}
+    )
   end
 end
