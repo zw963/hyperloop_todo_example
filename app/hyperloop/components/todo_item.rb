1d62dd2168f0cfde5752a92a55be21fbab068573
Billy.Zheng <zw963@163.com>
2017-07-30 19:13:06 +0800

    为 todo_item 添加了一个代表正在编辑的状态:
    - 根据这个状态, 我们知道当前用户是否正在编辑.
    - 编辑状态的切换, 会自动刷新 components

--- app/hyperloop/components/todo_item.rb
+++ app/hyperloop/components/todo_item.rb
@@ -1,10 +1,16 @@
 class TodoItem < Hyperloop::Component
   param :todo
+  state editing: false
+
   render(LI) do
-    INPUT(type: :checkbox, checked: params.todo.completed).on(:click) do |evt|
-      params.todo.update(completed: !params.todo.completed)
+    if state.editing
+      EditItem(todo: params.todo)
+    else
+      INPUT(type: :checkbox, checked: params.todo.completed).on(:click) do
+        params.todo.update(completed: !params.todo.completed)
+      end
+      LABEL { params.todo.title }.on(:double_click) { mutate.editing true }
+      A { ' -X-' }.on(:click) { params.todo.destroy }
     end
-    EditItem(todo: params.todo)
-    A { ' -X-' }.on(:click) { params.todo.destroy }
   end
 end
