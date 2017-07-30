5ff2b7e856cdd1b5edb3ec07a6e08c25f45b0eae
Billy.Zheng <zw963@163.com>
2017-07-30 21:02:44 +0800

    使用更清晰的方式重构.

--- app/hyperloop/components/edit_item.rb
+++ app/hyperloop/components/edit_item.rb
@@ -5,8 +5,8 @@ class EditItem < Hyperloop::Component
   # 而下面的 on_??? 则是定义了两个 `自定义事件',
   # type: Proc 表示这个名称的 param 是个 event 类型的 params
   # 当这个 params.on_save 被调用时, 该事件的 handler(Proc对象) 会被执行.
-  param :on_save, type: Proc
-  param :on_cancel, type: Proc
+  param :on_save
+  param :on_cancel
 
   # dom_node 方法返回一个组件被 mount 成功后, 这个组件对应的 node.
   # 当 EditItem 组件 mount 成功后, 设定焦点.
@@ -17,10 +17,10 @@ class EditItem < Hyperloop::Component
     edit_input.on(:key_down) do |evt|
       next unless evt.key_code == 13
       params.todo.update(title: evt.target.value)
-      params.on_save            # 激活 on_save 自定义事件.
+      params.on_save.call            # 激活 on_save 自定义事件.
       # 因为声明的时候, 使用了 type: Proc, 意味着这个调用会自动在 params.on_save 之上调用 call 方法.
     end
-    edit_input.on(:blur) { params.on_cancel } # add
+    edit_input.on(:blur) { params.on_cancel.call } # add
     # edit_input.on(:blur) do |evt|
     #   params.todo.update(title: evt.target.value)
     # end
